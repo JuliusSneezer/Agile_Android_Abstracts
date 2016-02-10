01fb1cc83973a52341769e00c19181c9b5a2f7fb
ismailmare <ismailmare@Ismails-MacBook-Air.local>
2016-02-10 01:15:49 -0700

    Beginning next phase of Project Part 2

---
+++ WarOnDemand/app/src/androidTest/java/com/ismailmare/warondemand/Bidding/BiddingUseCaseTest.java
@@ -0,0 +1,7 @@
+package com.ismailmare.warondemand.Bidding;
+
+/**
+ * Created by ismailmare on 16-02-10.
+ */
+public class BiddingUseCaseTest {
+}
---
+++ WarOnDemand/app/src/androidTest/java/com/ismailmare/warondemand/Borrowing/BorrowingUseCase.java
@@ -0,0 +1,7 @@
+package com.ismailmare.warondemand.Borrowing;
+
+/**
+ * Created by ismailmare on 16-02-10.
+ */
+public class BorrowingUseCase {
+}
