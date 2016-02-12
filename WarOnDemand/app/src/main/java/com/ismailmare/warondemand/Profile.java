96b707fc4bdf8c1db2553864b4c665180d3f2992
Whitishmeteor <hegburgers-are-fat-free@live.ca>
2016-02-11 20:19:41 -0700

    some search cases added

---
+++ WarOnDemand/app/src/main/java/com/ismailmare/warondemand/Profile.java
@@ -0,0 +1,20 @@
+package com.ismailmare.warondemand;
+
+/**
+ * Created by Chris on 2/11/2016.
+ */
+//dummy class for tests
+public class Profile {
+
+    public void createProfile(String Username, String PhoneNumber) {
+        //dummy function for tests
+    }
+
+    public void editProfile(String PhoneNumber) {
+        //dummy function for tests
+    }
+
+    public String returnContactInfo() {
+        return null;
+    }
+}
---
+++ WarOnDemand/app/src/main/java/com/ismailmare/warondemand/Searching.java
@@ -0,0 +1,23 @@
+package com.ismailmare.warondemand;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Searching extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_searching);
+
+    }
+
+    public void setKeywords(String keywords){
+        //dummy function for test cases
+    }
+
+}
