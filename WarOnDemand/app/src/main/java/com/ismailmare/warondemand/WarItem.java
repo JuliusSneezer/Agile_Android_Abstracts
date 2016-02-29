fb41bdf9056ed580a52bcab7155a848585af18dd
Koukoula <unknown.essence@hotmail.com>
2016-02-29 00:25:50 -0700

    Updated user to match UML

--- WarOnDemand/app/src/main/java/com/ismailmare/warondemand/WarItem.java
+++ WarOnDemand/app/src/main/java/com/ismailmare/warondemand/WarItem.java
@@ -1,29 +1,47 @@
 package com.ismailmare.warondemand;
 
+import java.util.ArrayList;
+
 /**
  * Created by unkno on 2016-02-11.
  */
 public class WarItem {
     private String name;
-    private String desc;
 
+    private Double cost;
+
+    private String desc;
+    //Using an int for ease of coding in status.
     //0 for Available
     //1 for bid on
     //2 for borrowed
     private int status;
-    //private Boolean returning;//if true, set to available when returned.
 
-    public WarItem(String name, String desc) {
+    private Boolean autoSetAvailable;
+    private Boolean bidOn;
+    private ArrayList<User> listOfBidders;
+    private User owner;
+    private User borrower;
+
+    public WarItem(String name, String desc, Double cost, User owner ) {
         this.name = name;
+        this.cost = cost;
         this.desc = desc;
         this.status = 0;
-        //this.returning = Boolean.FALSE;
+        this.autoSetAvailable = false;
+        this.owner = owner;
+
     }
 
-    public WarItem(String name) {
+
+    public WarItem(String name, Double cost, User owner) {
         this.name = name;
         this.desc = "Default Description";
         this.status = 0;
+        this.cost = cost;
+        this.status = 0;
+        this.autoSetAvailable = false;
+        this.owner = owner;
         //this.returning = Boolean.FALSE;
     }
 
@@ -52,11 +70,58 @@ public class WarItem {
         this.status = status;
     }
 
-/*    public Boolean getReturning() {
-        return returning;
+    public Double getCost() {
+        return cost;
     }
 
-    public void setReturning(Boolean returning) {
-        this.returning = returning;
-    }*/
+    public void setCost(Double cost) {
+        this.cost = cost;
+    }
+
+    public Boolean getAutoSetAvailable() {
+        return autoSetAvailable;
+    }
+
+    public void setAutoSetAvailable(Boolean autoSetAvailable) {
+        this.autoSetAvailable = autoSetAvailable;
+    }
+
+    public Boolean getBidOn() {
+        return bidOn;
+    }
+
+    public void setBidOn(Boolean bidOn) {
+        this.bidOn = bidOn;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public User getBorrower() {
+        return borrower;
+    }
+
+    public void setBorrower(User borrower) {
+        this.borrower = borrower;
+    }
+
+    public ArrayList<User> getListOfBidders() {
+        return listOfBidders;
+    }
+
+    public void setListOfBidders(ArrayList<User> listOfBidders) {
+        this.listOfBidders = listOfBidders;
+    }
+
+    public void AddBidder(User userAdded) {
+        this.listOfBidders.add(userAdded);
+    }
+    public void RemoveBidder(User userRemoved){
+        this.listOfBidders.remove(userRemoved);
+    }
 }
